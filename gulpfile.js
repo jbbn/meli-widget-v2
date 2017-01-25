c6ec827c056a87805b52df5b3f139c9eae158f54
Joao BBN <joaobbn@gmail.com>
2017-01-25 16:41:48 -0200

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var csso = require('gulp-csso');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var clean = require('gulp-clean');
+var ghPages = require('gulp-gh-pages');
+
+// Styles
+gulp.task('styles', function () {
+    var sassPath = 'src/styles/main.scss';
+    return sass(sassPath, {
+            style: 'expanded'
+        })
+        .pipe(autoprefixer('last 1 version'))
+        .pipe(gulp.dest('src/styles'));
+});
+
+// Scripts
+gulp.task('scripts', function () {
+    return gulp.src('src/scripts/**/*.js');
+        //.pipe(jshint('.jshintrc'))
+        //.pipe(jshint.reporter('default'));
+});
+
+// HTML
+gulp.task('html', ['styles', 'scripts'], function () {
+    return gulp.src('src/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', csso()))
+        .pipe(gulp.dest('dist'));
+});
+
+// Clean
+gulp.task('clean', function () {
+    return gulp.src(['dist/*.css', 'dist/*.js'], {
+        read: false
+    }).pipe(clean());
+});
+
+// Build
+gulp.task('build', ['html']);
+
+// Default - clean and build
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
+
+// Watch
+gulp.task('watch', function () {
+
+    // Watch .scss files
+    gulp.watch('src/styles/**/*.scss', ['styles']);
+
+    // Watch .js files
+    gulp.watch('src/scripts/**/*.js', ['scripts']);
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
